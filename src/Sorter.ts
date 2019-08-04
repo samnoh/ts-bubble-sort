1ff17c8c5f8d99f3780f201d604327a683246268
Sam Noh <samnoh21@outlook.com>
2019-08-04 21:08:14 +1200

    add - Sortable interface

--- src/Sorter.ts
+++ src/Sorter.ts
@@ -1,7 +1,12 @@
-import { NumbersCollection } from './helpers/NumbersCollection';
+interface Sortable {
+    data: number[];
+    length: number;
+    compare(leftIndex: number, rightIndex: number): boolean;
+    swap(leftIndex: number, rightIndex: number): void;
+}
 
 export class Sorter {
-    constructor(public collection: NumbersCollection) {}
+    constructor(public collection: Sortable) {}
 
     sort(): void {
         const length = this.collection.length;
